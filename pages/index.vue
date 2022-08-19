814bf6f61da245697806e344f2258b0f70e2a382
Emily Vinsen <emilyv@flocc.co>
2022-08-19 10:38:15 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    <h1>Playground home page</h1>
+    <List :posts="posts" />
+  </div>
+</template>
+
+<script>
+export default {
+  data:() =>({
+    posts: []
+  }),
+  mounted() {
+    this.posts = [
+      { title: 'test1', id: 1 },
+      { title: 'test2', id: 2 },
+    ];
+  },
+};
+</script>
